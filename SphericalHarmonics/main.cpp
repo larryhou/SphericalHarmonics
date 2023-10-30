7265511551c74ee3c14de7167ea3a7e0209f2778
larryhou <larryhou@tencent.com>
2023-10-30 17:18:13 +0800

    project intialization

---
+++ SphericalHarmonics/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SphericalHarmonics
+//
+//  Created by larryhou on 2023/10/30.
+//
+
+#include <iostream>
+#include <fbxsdk.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
